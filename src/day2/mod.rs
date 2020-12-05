463605807721a91d4a8368b4d2704692f51947ca
Kevin Oberlies <favilo@gmail.com>
2020-12-04 16:39:19 -0800

    Starting to remove cargo-aoc, so I can use better return types and lifetimes

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,53 +1,50 @@
-use aoc_runner_derive::{aoc, aoc_generator};
-
 use anyhow::Result;
 use regex::Regex;
 
+use crate::Runner;
+
+pub struct Day02;
+
 type Pair = ((usize, usize), u8);
-type Input = Vec<(Pair, Vec<u8>)>;
+impl Runner for Day02 {
+    type Input = Vec<(Pair, Vec<u8>)>;
+    type Output = usize;
 
-#[aoc_generator(day2)]
-fn get_input(input: &str) -> Result<Input> {
-    let lines = input.lines();
-    let re = Regex::new(r"(\d+)-(\d+) (.): (.+)").unwrap();
-    let lines = lines.filter(|l| re.is_match(&l)).map(|line| {
-        let caps = re.captures(&line).unwrap();
-        let start = caps[1].parse::<usize>().unwrap();
-        let end = caps[2].parse::<usize>().unwrap();
-        let c = caps[3].parse::<char>().unwrap() as u8;
-        let pass = caps[4].to_owned();
-        (((start, end), c), pass.into_bytes())
-    });
-    let lines = lines.collect::<Input>();
-    log::info!("{}", lines.len());
-    Ok(lines)
-}
+    fn day() -> usize {
+        2
+    }
 
-#[aoc(day2, part1)]
-fn part1(input: &Input) -> Result<usize> {
-    let correct = input.into_iter().filter(|&(((s, e), c), pass)| -> bool {
-        (*s..=*e).contains(&pass.iter().filter(|o| o == &c).count())
-    });
-    Ok(correct.count())
-}
+    #[inline]
+    fn get_input(input: &str) -> Result<Self::Input> {
+        let lines = input.lines();
+        let re = Regex::new(r"(\d+)-(\d+) (.): (.+)").unwrap();
+        let lines = lines.filter(|l| re.is_match(&l)).map(|line| {
+            let caps = re.captures(&line).unwrap();
+            let start = caps[1].parse::<usize>().unwrap();
+            let end = caps[2].parse::<usize>().unwrap();
+            let c = caps[3].parse::<char>().unwrap() as u8;
+            let pass = caps[4].to_owned();
+            (((start, end), c), pass.into_bytes())
+        });
+        let lines = lines.collect::<Self::Input>();
+        log::info!("{}", lines.len());
+        Ok(lines)
+    }
 
-#[aoc(day2, part1, fold)]
-fn part1_fold(input: &Input) -> Result<usize> {
-    let correct = input.into_iter().filter(|&(((s, e), c), pass)| -> bool {
-        (*s..=*e).contains(
-            &pass
-                .iter()
-                .fold(0, |acc, o| if o == c { acc + 1 } else { acc }),
-        )
-    });
-    Ok(correct.count())
-}
+    #[inline]
+    fn part1(input: &Self::Input) -> Result<usize> {
+        let correct = input.into_iter().filter(|&(((s, e), c), pass)| -> bool {
+            (*s..=*e).contains(&pass.iter().filter(|o| o == &c).count())
+        });
+        Ok(correct.count())
+    }
 
-#[aoc(day2, part2)]
-fn part2(input: &Input) -> Result<usize> {
-    let correct = input
-        .into_iter()
-        .map(|(((f, s), c), ref pass)| (pass[*f - 1] == *c as u8, pass[*s - 1] == *c as u8))
-        .filter(|(a, b)| a != b);
-    Ok(correct.count())
+    #[inline]
+    fn part2(input: &Self::Input) -> Result<usize> {
+        let correct = input
+            .into_iter()
+            .map(|(((f, s), c), ref pass)| (pass[*f - 1] == *c as u8, pass[*s - 1] == *c as u8))
+            .filter(|(a, b)| a != b);
+        Ok(correct.count())
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,46 @@
+use std::{fmt::Debug, fs::read_to_string, time::Instant};
+
+use anyhow::Result;
+
 pub mod day1;
 pub mod day2;
-pub mod day3;
-pub mod day4;
+// pub mod day3;
+// pub mod day4;
 
-aoc_runner_derive::aoc_lib! { year = 2020 }
+pub trait Runner {
+    type Input;
+    type Output: Debug;
+
+    fn run() -> Result<()> {
+        let input = read_to_string(format!("input/2020/day{}.txt", Self::day()))?;
+        let input = Self::get_input(&input)?;
+        let now = Instant::now();
+
+        let output1 = Self::part1(&input)?;
+        let elapsed1 = now.elapsed();
+
+        let now = Instant::now();
+        let output2 = Self::part2(&input)?;
+        let elapsed2 = now.elapsed();
+
+        log::info!("Part 1 - {:?}", output1);
+        log::info!("Took {:?}", elapsed1);
+        log::info!("Part 2 - {:?}", output2);
+        log::info!("Took {:?}", elapsed2);
+        Ok(())
+    }
+
+    fn day() -> usize;
+
+    fn get_input(_: &str) -> Result<Self::Input>;
+    fn part1(_: &Self::Input) -> Result<Self::Output>;
+    fn part2(_: &Self::Input) -> Result<Self::Output>;
+}
+
+pub fn run() -> Result<()> {
+    day1::Day01::run()?;
+    day2::Day02::run()?;
+    // day3::Runner::run()?;
+    // day4::Runner::run()?;
+    Ok(())
+}
